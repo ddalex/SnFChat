f1ca5098864704c1526422fbc46d9a2212b8a481
Alex Damian <ddalex@gmail.com>
2013-01-09 21:13:56 +0200

    Android layout of main activity done
    We commit the layout of the main activity

--- android/SnFChat/src/ro/ddalex/snfchat/MainActivity.java
+++ android/SnFChat/src/ro/ddalex/snfchat/MainActivity.java
@@ -1,15 +1,30 @@
 package ro.ddalex.snfchat;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	ArrayList<String> listItems=new ArrayList<String>();
+	ArrayAdapter<String> adapter;
+	int count = 0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ListView listView =  (ListView)findViewById(R.id.list);
+	    adapter=new ArrayAdapter<String>(this,
+	               android.R.layout.simple_list_item_1,
+	               listItems);
+	    listView.setAdapter(adapter);
 	}
 
 	@Override
@@ -19,4 +34,12 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	
+	public void send_message(View view)
+	{
+		// for now, just post up
+		listItems.add("dada:test " + count);
+		count ++;
+		adapter.notifyDataSetChanged();
+	}
 }
